5b3a50312fd036d23ed420e497b761849f9432e0
Omar Davidson <omardavidson227@gmail.com>
2024-07-31 23:56:19 -0400

    starting from scratch

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+// userInput is initialized as 0; The user will only enter 3 or 6; if the user inputs a number that is not 3 or 6 we need to handle that
+var userInput = 0; 
+var hole = 1
+var userName = ""
+userName = prompt(`Welcome! Let's play some golf! What's your name?`)
+userInput = prompt(`Hello, ${userName}! How many holes of golf would you like to play? (3 or 6)`)

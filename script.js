4f631ea03b88ab6fdcccbf0e3e426856ebf78a60
Omar Davidson <omardavidson227@gmail.com>
2024-08-01 18:56:16 -0400

    finalizing project two; checking code is functioning properly

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ let coursePar1 = 9;
 // Establising the course par for a userInput of 3
 let coursePar2 = 18;
 
+var threeHoleFinalScore = 0
+
+var sixHoleFinalScore = 0
+
 // promting user for their name
 userName = prompt(`Welcome! Let's play some golf! What's your name?`);
 // promting user for their perfered numerer of holes
@@ -21,19 +25,40 @@ chosenNumOfHoles = prompt(`Hello, ${userName}! How many holes of golf would you 
 while (hole < chosenNumOfHoles) {
     // incrementing hole counter by 1
     ++hole;
-    // 
-    var incrementingScore = prompt(`How many putts for hole ${hole}?`);
-    scoreArray.push(incrementingScore);
+    var incrementingScore = prompt(`How many putts for hole ${hole}? (Par 3)`);
+    scoreArray.push(parseInt(incrementingScore));
 }
 
-for (element in scoreArray) {
-    ++element
+if (hole === 3) {
+    userScoreTotal = scoreArray[0] + scoreArray[1] +scoreArray[2];
+    console.log(`Raw points total for 3 holes: ${userScoreTotal}`);
+    threeHoleFinalScore =  userScoreTotal - coursePar1;
+    console.log(`Good game, ${userName}! Your total par was: ${threeHoleFinalScore}`);
+} 
+
+if (hole === 6) {
+    userScoreTotal = scoreArray[0] + scoreArray[1] +scoreArray[2] + scoreArray[3] + scoreArray[4] +scoreArray[5];
+    console.log(`Raw points total for 6 holes: ${userScoreTotal}`);
+    sixHoleFinalScore = userScoreTotal - coursePar2;
+    console.log(sixHoleFinalScore);
+    console.log(`Good game, ${userName}! Your total par was: ${sixHoleFinalScore}`);
 }
 
-// console.log(userScoreTotal)
-// 
 
-console.log(scoreArray)
-console.log(userScoreTotal);
-// 6 (userScore) - 9 (coursePar) 
-// console.log(`User's score is: ${userScore - coursePar1}`) 
+/*
+if (threeHoleFinalScore > 0) {
+    console.log(`Better luck next time, ugh! Your total par was: +${threeHoleFinalScore}`)
+} else if (threeHoleFinalScore < 0) {
+    console.log(`Wow! You did really well! Your total par was: ${threeHoleFinalScore}`)
+} else {
+    console.log(`Yeah, ${userName}! Not bad! You got a solid score! Your total par was: ${threeHoleFinalScore}`)
+}
+
+if (sixHoleFinalScore > 0) {
+    console.log(`Better luck next time, ugh! Your total par was: +${sixHoleFinalScore}`)
+} else if (threeHoleFinalScore < 0) {
+    console.log(`Wow! You did really well! Your total par was: ${sixHoleFinalScore}`)
+} else {
+    console.log(`Yeah, ${userName}! Not bad! You got a solid score! Your total par was: ${sixHoleFinalScore}`)
+}
+*/

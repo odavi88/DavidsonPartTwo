940bee229a8bfb1039e77186984243c7d7f76110
Omar Davidson <omardavidson227@gmail.com>
2024-08-02 08:33:18 -0400

    cleaning up code and submitting project 2

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var chosenNumOfHoles = 0;
 // Storing the hole count here
 var hole = 0;
 //  user score will increase with each user input; if the user inputs 1; 2; 3; for respective putts hit per hole then user socre will equal 6
-var rawScore = 0;
+var rawTotalScore = 0;
 // User will input their name updating this 
 var userName = "";
 // Establising the course par for a userInput of 3
@@ -11,7 +11,7 @@ var scoreArray = []
 let coursePar1 = 9;
 // Establising the course par for a userInput of 3
 let coursePar2 = 18;
-var finalScore = 0
+var finalScore = 0;
 
 // promting user for their name
 userName = prompt(`Welcome! Let's play some golf! What's your name?`);
@@ -23,34 +23,38 @@ chosenNumOfHoles = prompt(`Hello, ${userName}! How many holes of golf would you 
 while (hole < chosenNumOfHoles) {
     // incrementing hole counter by 1
     ++hole;
+    // initializing a property "incrementingScore" to store the promt I want to run the same amount of times as the chosenNumOfHoles input
     var incrementingScore = prompt(`How many putts for hole ${hole}? (Par 3)`);
+    // appending the incrementingScore into the scoreArray while conveting the string into an integer
     scoreArray.push(parseInt(incrementingScore));
-
 }
 
+// looping over scoreArray to extract its elements, adding those into the rawTotalScore property
 scoreArray.forEach(element => {
-    rawScore += element
+    rawTotalScore += element
 });
 
-console.log(rawScore)
-
-if (hole === 3 && rawScore === coursePar1) {
-    finalScore =  rawScore - coursePar1; /* 9 - 9 = 0 */
-    console.log(`Good game, ${userName}! Your total par was: ${finalScore}`);
-} else if (hole === 3 && rawScore > coursePar1) {
-    finalScore =  rawScore - coursePar1;
+// if "hole" is equal to 3 and the rawTotalScore is equal to the course par for 3 holes (9)...
+if (hole === 3 && rawTotalScore === coursePar1) {
+    // then I update "finalScore" with the calculation here
+    finalScore = rawTotalScore - coursePar1; /* 9 - 9 = 0 */
+    console.log(`Good game, ${userName}! Your total par was: ${finalScore}.`);
+    //  if "hole" is equal to 3 and the rawTotalScore is greater than the course par...
+} else if (hole === 3 && rawTotalScore > coursePar1) {
+    finalScore = rawTotalScore - coursePar1; /* 10 - 9 = 1*/
     console.log(`Nice try, ${userName}... Your total par was: +${finalScore}.`);
-} else if (hole === 3 && rawScore < coursePar1) {
-    console.log(`Raw points total for 3 holes: ${rawScore}`);
-    finalScore =  rawScore - coursePar1;
-    console.log(`Great job, ${userName}! Your total par was: ${finalScore}.`);
-} else if (hole === 6 && rawScore === coursePar2) {
-    finalScore =  rawScore - coursePar2;
-    console.log(`Good game, ${userName}! Your total par was: ${finalScore}`);
-} else if (hole === 6 && rawScore > coursePar2) {
-    finalScore =  rawScore - coursePar2;
+    //  if if "hole" is equal to 3 and the rawTotalScore is less than the course par...
+} else if (hole === 3 && rawTotalScore < coursePar1) {
+    console.log(`Raw points total for 3 holes: ${rawTotalScore}`);
+    finalScore = rawTotalScore - coursePar1; /* 8 - 9 = -1*/
+    console.log(`Great job, ${userName}! Your total par was: ${finalScore}!`);
+} else if (hole === 6 && rawTotalScore === coursePar2) {
+    finalScore = rawTotalScore - coursePar2;
+    console.log(`Good game, ${userName}! Your total par was: ${finalScore}.`);
+} else if (hole === 6 && rawTotalScore > coursePar2) {
+    finalScore = rawTotalScore - coursePar2;
     console.log(`Nice try, ${userName}... Your total par was: +${finalScore}.`);
-} else if (hole === 6 && rawScore < coursePar2) {
-    finalScore =  rawScore - coursePar2;
-    console.log(`Great job, ${userName}! Your total par was: ${finalScore}.`);
+} else if (hole === 6 && rawTotalScore < coursePar2) {
+    finalScore = rawTotalScore - coursePar2;
+    console.log(`Great job, ${userName}! Your total par was: ${finalScore}!`);
 }
